f095d5942a04e905562e8bd8bd8d02b9b1f7c5ab
niteshrestha <4751915+niteshrestha@users.noreply.github.com>
2020-05-10 10:10:06 +0545

    initial project

---
+++ api/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from django.core import serializers
+from django.conf import settings
+import json
+
+
+@api_view(['GET'])
+def greeter(request):
+    try:
+        response = {"message": "Hello, World! This is work in progress."}
+        return JsonResponse(response)
+    except ValueError as e:
+        return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
